55afc796b0a52a51b312e8481309ec26538a7464
kyudong3 <kyudong3@gmail.com>
2017-11-01 16:44:12 +0900

    게시판 글쓰기 , 수정 , 삭제

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -2,15 +2,17 @@ package com.board.controller;
 
 import com.board.entity.BoardPost;
 import com.board.service.BoardPostService;
+import com.sun.tools.javac.code.Type;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.lang.model.type.ErrorType;
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -18,10 +20,12 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping(path = "/board")
 public class BoardController {
 
     @Autowired
     BoardPostService boardPostService;
+
 
     @GetMapping
     public String list(Model model) {
@@ -30,20 +34,66 @@ public class BoardController {
         return "/board/list";
     }
 
-    @GetMapping(path = "/board/list")
+    // 전체 게시판 글 가져오기 //
+    @GetMapping(path = "/list")
     public List<BoardPost> showList(Model model) {
         List<BoardPost> boardPosts = boardPostService.findAll();
         return boardPosts;
     }
 
-    @PostMapping(path = "/board/create")
-    public String create(@Validated BoardPost boardPost, BindingResult bindingResult, Model model) {
+    // seqNo에 해당하는 게시글 가져오기 //
+    @GetMapping(path = "/{seqNo}")
+    public ResponseEntity<?> getBoard(@PathVariable("seqNo") int seqNo) {
+        BoardPost findSeqNo = boardPostService.findOne(seqNo);
+        if(findSeqNo == null) {
+            return new ResponseEntity("no seqNo found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<BoardPost>(findSeqNo, HttpStatus.OK);
+    }
+
+    // 게시판 글쓰기 //
+    @PostMapping(path = "/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@Valid @RequestBody BoardPost boardPost, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()) {
             return list(model);
         }
 
-        BoardPost boardPost1 = new BoardPost();
-        boardPostService.create(boardPost1);
+        boardPostService.create(boardPost);
         return "redirect:/board";
     }
+
+    // seqNo에 해당하는 게시글 수정하기 //
+    @PostMapping(path = "/edit/{seqNo}")
+    public String update(@PathVariable("seqNo") int seqNo, @RequestBody BoardPost boardPost) {
+        BoardPost getBoard = boardPostService.findOne(seqNo);
+        if(getBoard == null) {
+            return "No seqNo Found!!";
+        }
+
+        getBoard.setTitle(boardPost.getTitle());
+        getBoard.setContent(boardPost.getContent());
+        getBoard.setHit_count(boardPost.getHit_count());
+        getBoard.setRegDate(boardPost.getRegDate());
+        getBoard.setUpdateDate(boardPost.getUpdateDate());
+
+        boardPostService.update(getBoard);
+
+        return "redirect:/board";
+    }
+
+    // seqNo에 해당하는 게시글 삭제 //
+    @DeleteMapping(path = "/{seqNo}")
+    public String deleteBoard(@PathVariable("seqNo") int seqNo) {
+        BoardPost findBoard = boardPostService.findOne(seqNo);
+
+        if (findBoard == null) {
+            return "Unable to delete. seqNo not found.";
+        }
+
+        boardPostService.delete(seqNo);
+
+        return "redirect:/board";
+    }
+
 }
